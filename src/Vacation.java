495ab0a339eb1788738a967cbb424c51762f50ce
MahiGitH <mahiadamu1@gmail.com>
2022-05-02 15:52:28 -0400

    first commit

---
+++ src/Vacation.java
@@ -0,0 +1,7 @@
+public class Vacation {
+  public static void main(String[] args) {
+    System.out.println("I would like to vist");
+    System.out.println("Tokyo");
+    System.out.println("Japan");
+}
+}
